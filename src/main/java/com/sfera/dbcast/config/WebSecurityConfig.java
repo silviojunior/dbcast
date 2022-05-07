2058670dfc602862cf77fc142996f5ae599fda84
Silvio Junior <silvio.fs.junior@gmail.com>
2022-05-07 00:02:17 -0300

    Enable cors and adding static files

--- src/main/java/com/sfera/dbcast/config/WebSecurityConfig.java
+++ src/main/java/com/sfera/dbcast/config/WebSecurityConfig.java
@@ -2,12 +2,10 @@ package com.sfera.dbcast.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
@@ -18,18 +16,9 @@ import java.util.Arrays;
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
-    private static final String[] PUBLIC_MATCHERS_GET_ONLY = {"/actor"};
-
-    private static final String[] PUBLIC_MATCHERS_POST_ONLY = { "/actor"};
-
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.cors().and().csrf().disable();
-        http.authorizeRequests()
-                .antMatchers(HttpMethod.POST, PUBLIC_MATCHERS_POST_ONLY).permitAll()
-                .antMatchers(HttpMethod.GET, PUBLIC_MATCHERS_GET_ONLY).permitAll()
-                .anyRequest().authenticated();
-        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
     }
 
     @Override
@@ -47,4 +36,5 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         source.registerCorsConfiguration("/**", configuration);
         return source;
     }
+
 }
